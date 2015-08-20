929ea2e576857be9f5fdce1f8f68a38f3861f3b4
Shohboz <vip.shohboz@gmail.com>
2015-08-21 01:39:57 +0300

    Add some modal.

---
+++ app/assets/javascripts/leanModal.js
@@ -0,0 +1,99 @@
+(function($) {
+    var _stack = 0,
+    _lastID = 0,
+    _generateID = function() {
+      //_lastID++;
+      return 'materialize-lean-overlay-' + _lastID;
+    };
+
+  $.fn.extend({
+    openModal: function(options) {
+
+      $('body').css('overflow', 'hidden');
+
+      var defaults = {
+        opacity: 0.5,
+        in_duration: 350,
+        out_duration: 250,
+        ready: undefined,
+        complete: undefined,
+        dismissible: true,
+        starting_top: '4%'
+      },
+      overlayID = _generateID(),
+      $modal = $(this),
+      $overlay = $('<div class="lean-overlay"></div>'),
+      //lStack = (++_stack);
+      lStack = _stack;
+
+      // Store a reference of the overlay
+      $overlay.attr('id', overlayID).css('z-index', 1000 + lStack * 2);
+      $modal.data('overlay-id', overlayID).css('z-index', 1000 + lStack * 2 + 1);
+      // Fix multiple overlay
+      if (!$('body').has('.lean-overlay').length){
+        $("body").append($overlay);
+      }
+
+      // Override defaults
+      options = $.extend(defaults, options);
+
+      if (options.dismissible) {
+        $overlay.click(function() {
+          $modal.closeModal(options);
+        });
+        // Return on ESC
+        $(document).on('keyup.leanModal' + overlayID, function(e) {
+          if (e.keyCode === 27) {   // ESC key
+            $modal.closeModal(options);
+          }
+        });
+      }
+
+      $modal.find(".modal-close").on('click.close', function(e) {
+        $modal.closeModal(options);
+      });
+
+      $overlay.css({ display : "block", opacity : 0 });
+
+      $modal.css({
+        display : "block",
+        opacity: 0
+      });
+
+      $overlay.velocity({opacity: options.opacity}, {duration: options.in_duration, queue: false, ease: "easeOutCubic"});
+      $modal.data('associated-overlay', $overlay[0]);
+
+      // Define Bottom Sheet animation
+      if ($modal.hasClass('bottom-sheet')) {
+        $modal.velocity({bottom: "0", opacity: 1}, {
+          duration: options.in_duration,
+          queue: false,
+          ease: "easeOutCubic",
+          // Handle modal ready callback
+          complete: function() {
+            if (typeof(options.ready) === "function") {
+              options.ready();
+            }
+          }
+        });
+      }
+      else {
+        $.Velocity.hook($modal, "scaleX", 0.7);
+        $modal.css({ top: options.starting_top });
+        $modal.velocity({top: "10%", opacity: 1, scaleX: '1'}, {
+          duration: options.in_duration,
+          queue: false,
+          ease: "easeOutCubic",
+          // Handle modal ready callback
+          complete: function() {
+            if (typeof(options.ready) === "function") {
+              options.ready();
+            }
+          }
+        });
+      }
+
+
+    }
+  });
+})(jQuery);
