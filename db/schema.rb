0ea6b791445f472b1ba0c65a203bc0b037078104
Shohboz <vip.shohboz@gmail.com>
2015-09-17 22:45:29 +0300

    Remove image_url column.

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 20150910203737) do
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
